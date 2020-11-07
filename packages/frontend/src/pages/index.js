0b84f619a0a182a36613cc8aa91dd4e55cd4c481
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2020-11-08 01:14:59 +0500

    link

--- packages/frontend/src/pages/index.js
+++ packages/frontend/src/pages/index.js
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
-import { Button, Container, Flex, Heading, Link } from "theme-ui";
+import { Button, Container, Flex, Heading, NavLink } from "theme-ui";
 import { IdentityContext } from "../../netlifyIdentityContext";
 import Navbar from "../components/Navbar";
-
+import { Link } from "gatsby";
 
 const Index = () => {
   const context = useContext(IdentityContext);
@@ -13,15 +13,21 @@ const Index = () => {
       <Navbar />
       <Flex sx={{ flexDirection: "column", padding: 3 }}>
         <Heading as="h1"> Todo App, Login to get Started.</Heading>
-        {!user && <Button
-          sx={{ marginTop: 2, color: "black" }}
-          onClick={() => {
-            netlifyIdentity.open();
-          }}
-        >
-          Login
-        </Button>}
-        {user && <Link sx={{fontSize:'4rem'}} to="/app">Go To Todos Dashboard</Link>}
+        {!user && (
+          <Button
+            sx={{ marginTop: 2, color: "black" }}
+            onClick={() => {
+              netlifyIdentity.open();
+            }}
+          >
+            Login
+          </Button>
+        )}
+        {user && (
+          <NavLink as={Link} sx={{ fontSize: "4rem" }} to="/app">
+            Go To Todos Dashboard
+          </NavLink>
+        )}
       </Flex>
     </Container>
   );
